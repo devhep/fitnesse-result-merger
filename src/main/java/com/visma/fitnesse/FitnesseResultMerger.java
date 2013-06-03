3f0d4af6fd51e0160b04292f9e1c6582690c72f4
Hans Egil Pettersen <hans.egil.pettersen@visma.com>
2013-06-03 15:04:40 +0200

    First version

---
+++ src/main/java/com/visma/fitnesse/FitnesseResultMerger.java
@@ -0,0 +1,73 @@
+package com.visma.fitnesse;
+
+import com.visma.fitnesse.schema.ObjectFactory;
+import com.visma.fitnesse.schema.TestResults;
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ * Hello world!
+ *
+ */
+public class FitnesseResultMerger 
+{
+    private static JAXBContext jAXBContext;
+    private static Unmarshaller jaxbUnmarshaller;
+    private static Marshaller jaxbMarshaller;
+    
+    private static TestResults mergedResults;
+
+    public static void main( String[] args ) throws JAXBException
+    {
+        jAXBContext = JAXBContext.newInstance(TestResults.class);
+        jaxbUnmarshaller = jAXBContext.createUnmarshaller();
+        jaxbMarshaller = jAXBContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        mergedResults = new ObjectFactory().createTestResults();
+        mergedResults.setFinalCounts(new ObjectFactory().createTestResultsFinalCounts());
+        
+        for(String arg: args){
+            TestResults testResults = parseFile(arg);
+            toString(testResults);
+            System.out.println("TestResults.rootPath: " + testResults.getRootPath());
+            System.out.println("TestResults.totalRunTimeMillis: " + testResults.getTotalRunTimeInMillis());  
+            mergeResult(testResults);
+        }
+        toString(mergedResults);
+        jaxbMarshaller.marshal(mergedResults, System.out);
+    }
+    
+    private static TestResults parseFile(String fileName) throws JAXBException{
+        File file = new File(fileName);
+        return (TestResults)jaxbUnmarshaller.unmarshal(file);
+    }
+    
+    private static void mergeResult(TestResults result){
+        mergedResults.setFitNesseVersion(result.getFitNesseVersion());
+        mergedResults.getResult().addAll(result.getResult());
+        mergedResults.setTotalRunTimeInMillis(mergedResults.getTotalRunTimeInMillis() + result.getTotalRunTimeInMillis());
+        mergedResults.setRootPath( mergedResults.getRootPath() == null ? result.getRootPath() : mergedResults.getRootPath() + ", " + result.getRootPath());
+        mergeFinalCounts(result.getFinalCounts());
+    }
+    
+    private static void mergeFinalCounts(TestResults.FinalCounts finalCounts){
+        TestResults.FinalCounts mergedFinalCounts = mergedResults.getFinalCounts();
+        mergedFinalCounts.setExceptions(mergedFinalCounts.getExceptions() + finalCounts.getExceptions());
+        mergedFinalCounts.setIgnores(mergedFinalCounts.getIgnores()+ finalCounts.getIgnores());
+        mergedFinalCounts.setRight(mergedFinalCounts.getRight()+ finalCounts.getRight());
+        mergedFinalCounts.setWrong(mergedFinalCounts.getWrong()+ finalCounts.getWrong());
+    }
+    
+    private static void toString(TestResults testResults){
+        System.out.println("===== rootPath: " + testResults.getRootPath());
+        System.out.println("totalRunTimeMillis: " + testResults.getTotalRunTimeInMillis());            
+        System.out.println("finalCounts.right: " + testResults.getFinalCounts().getRight());            
+        System.out.println("finalCounts.wrong: " + testResults.getFinalCounts().getWrong());            
+        System.out.println("finalCounts.exceptions: " + testResults.getFinalCounts().getExceptions());            
+        System.out.println("finalCounts.ignores: " + testResults.getFinalCounts().getIgnores());            
+    }
+}
